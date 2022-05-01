5ea631efb7bb7eb2577ab76317bcb7637bf535d1
gracchen <gracetheartist@g.ucla.edu>
2022-05-01 13:48:48 -0700

    final draft, formatted

--- index.js
+++ index.js
@@ -2,30 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
-/*This class is equivalent to the following function:
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}*/
-
 function Square(props) {
   return (
-      <button 
-        className="square" 
-        onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -37,129 +19,123 @@ class Board extends React.Component {
       OisSelected: false,
       squares: Array(9).fill(null),
       xIsNext: true, //default start as X
-      selectedPos: null,
+      selectedPos: null
     };
   }
-  
-  handleClick(i) { //is called when props.onClick in square
+
+  handleClick(i) {
+    //is called when props.onClick in square
     const squares = this.state.squares.slice();
     //squares[i] = this.state.xIsNext ? 'X' : 'O';
 
-    if (calculateWinner(squares)) { //do nothing if someone won
-     return;
+    if (calculateWinner(squares)) {
+      //do nothing if someone won
+      return;
     }
 
-    if (this.state.numSteps < 6)  //regular tic tac toe behavior until >6 steps
-    {
-      if (squares[i]) //do nothing if square already filled
+    if (this.state.numSteps < 6) {
+      //regular tic tac toe behavior until >6 steps
+      if (squares[i])
+        //do nothing if square already filled
         return;
 
       this.state.numSteps++;
 
-      if (this.state.xIsNext)
-      {
-        squares[i] = 'X';
-      } else
-      {
-        squares[i] = 'O';
+      if (this.state.xIsNext) {
+        squares[i] = "X";
+      } else {
+        squares[i] = "O";
       }
 
       this.setState({
-        squares: squares, 
-        xIsNext: !this.state.xIsNext, //flips xIsNext
+        squares: squares,
+        xIsNext: !this.state.xIsNext //flips xIsNext
       });
-    } 
-    else if (!this.state.XisSelected && this.state.xIsNext){  //now define chorus lapilli behavior if NOT YET selected item
-      if (squares[i] == 'X')
-      {
-         this.state.XisSelected = true;
-         this.state.selectedPos = i;  //store selected's position to check for adjacent-ness later
-         
-         this.setState({ squares: squares, });
-         squares[i] = null;
-        } 
-    }
-    else if (!this.state.OisSelected && !this.state.xIsNext){  //now define chorus lapilli behavior if NOT YET selected item
-      if (squares[i] == 'O')
-      {
-         this.state.OisSelected = true;
-         this.state.selectedPos = i;  //store selected's position to check for adjacent-ness later
-         
-         this.setState({ squares: squares, });
-         squares[i] = null;
-        } 
-    }
-    else  //if already selected, try to place it
-    {
-      if (i == this.state.selectedPos)  //if same place, place back down (always allowed)
-      {
-        squares[i] = this.state.xIsNext ? 'X' : 'O'; 
-        this.state.XisSelected = false;
-        this.state.OisSelected = false; 
-        this.setState({ squares: squares, }); //does NOT switch player's turn (compared to line 125)
-        return;
+    } else if (!this.state.XisSelected && this.state.xIsNext) {
+      //now define chorus lapilli behavior if NOT YET selected item
+      if (squares[i] == "X") {
+        this.state.XisSelected = true;
+        this.state.selectedPos = i; //store selected's position to check for adjacent-ness later
+
+        this.setState({ squares: squares });
+        squares[i] = null;
       }
-      else if (isAdjacent(i, this.state.selectedPos) && squares[i] == null)
-      {
-        if (squares[4] == 'X' && this.state.xIsNext)  //if X stepped in center in previous step, must move it out now
-        {
-          const whatif = this.state.squares.slice();
-          whatif[i] = 'X'
-  
-          if (calculateWinner(whatif))  //if player wins immediately with this move, allow it
-          {
-            this.setState({ squares: squares, });
-            squares[i] = 'X';  //place it down
-            this.state.XisSelected = false;
-            this.setState({ squares: squares,});
-            return;
-          }
-          else if (i != 4){  //only allow 4 
-            return;
-          }
-        }
-        else if (squares[4] == 'O' && !this.state.xIsNext)  //if O stepped in center in previous step, must move it out now
-        {
-          const whatif = this.state.squares.slice();
-          whatif[i] = 'O'
-  
-          if (calculateWinner(whatif))  //if player wins immediately with this move, allow it
-          {
-            this.setState({ squares: squares, });
-            squares[i] = 'O';  //place it down
-            this.state.OisSelected = false;
-            this.setState({ squares: squares,});
-            return;
-          }
-          else if (i != 4){  //only allow 4 
-            return;
-          }
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';  //place it down
+    } else if (!this.state.OisSelected && !this.state.xIsNext) {
+      //now define chorus lapilli behavior if NOT YET selected item
+      if (squares[i] == "O") {
+        this.state.OisSelected = true;
+        this.state.selectedPos = i; //store selected's position to check for adjacent-ness later
+
+        this.setState({ squares: squares });
+        squares[i] = null;
+      }
+    } //if already selected, try to place it
+    else {
+      if (i == this.state.selectedPos) {
+        //if same place, place back down (always allowed)
+        squares[i] = this.state.xIsNext ? "X" : "O";
         this.state.XisSelected = false;
         this.state.OisSelected = false;
-        this.setState({ squares: squares, xIsNext: !this.state.xIsNext,});
+        this.setState({ squares: squares }); //does NOT switch player's turn (compared to line 125)
+        return;
+      } else if (isAdjacent(i, this.state.selectedPos) && squares[i] == null) {
+        if (squares[4] == "X" && this.state.xIsNext) {
+          //if X stepped in center in previous step, must move it out now
+          const whatif = this.state.squares.slice();
+          whatif[i] = "X";
+
+          if (calculateWinner(whatif)) {
+            //if player wins immediately with this move, allow it
+            this.setState({ squares: squares });
+            squares[i] = "X"; //place it down
+            this.state.XisSelected = false;
+            this.setState({ squares: squares });
+            return;
+          } else if (i != 4) {
+            //only allow 4
+            return;
+          }
+        } else if (squares[4] == "O" && !this.state.xIsNext) {
+          //if O stepped in center in previous step, must move it out now
+          const whatif = this.state.squares.slice();
+          whatif[i] = "O";
+
+          if (calculateWinner(whatif)) {
+            //if player wins immediately with this move, allow it
+            this.setState({ squares: squares });
+            squares[i] = "O"; //place it down
+            this.state.OisSelected = false;
+            this.setState({ squares: squares });
+            return;
+          } else if (i != 4) {
+            //only allow 4
+            return;
+          }
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O"; //place it down
+        this.state.XisSelected = false;
+        this.state.OisSelected = false;
+        this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
       }
     }
   }
-  
+
   renderSquare(i) {
     return (
-      <Square 
-        value={this.state.squares[i]} 
+      <Square
+        value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
     );
   }
 
   render() {
-
     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = "Winner: " + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
     //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
@@ -216,7 +192,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -227,24 +203,14 @@ function calculateWinner(squares) {
   return null;
 }
 
-function isAdjacent(i, j)
-{
-  if (i === 0)
-    return j === 1 || j === 3 || j === 4;
-  else if (i === 1)
-    return j === 0 || j === 2 || j === 4 || j === 3 || j === 5;
-  else if (i === 2)
-    return j === 1 || j === 4 || j === 5;
-  else if (i === 3)
-    return j === 0 || j === 1 || j === 4 || j === 6 || j === 7;
-  else if (i === 4) 
-    return true;
-  else if (i === 5)
-    return j === 1 || j === 2 || j === 4 || j === 7 || j === 8;
-  else if (i === 6)
-    return j === 3 || j === 4 || j === 7;
-  else if (i === 7)
-    return j === 3 || j === 4 || j === 5 || j === 6 || j === 8;
-  else if (i === 8)
-    return j === 4 || j === 5 || j === 7;
+function isAdjacent(i, j) {
+  if (i === 0) return j === 1 || j === 3 || j === 4;
+  else if (i === 1) return j === 0 || j === 2 || j === 4 || j === 3 || j === 5;
+  else if (i === 2) return j === 1 || j === 4 || j === 5;
+  else if (i === 3) return j === 0 || j === 1 || j === 4 || j === 6 || j === 7;
+  else if (i === 4) return true;
+  else if (i === 5) return j === 1 || j === 2 || j === 4 || j === 7 || j === 8;
+  else if (i === 6) return j === 3 || j === 4 || j === 7;
+  else if (i === 7) return j === 3 || j === 4 || j === 5 || j === 6 || j === 8;
+  else if (i === 8) return j === 4 || j === 5 || j === 7;
 }
